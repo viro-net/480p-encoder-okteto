db883fe9550cbb5ee99938f014eb1a503f9c1001
ZxScripter <131220168+ZxScripter@users.noreply.github.com>
2023-08-02 00:38:16 +0530

    display_progress.py

--- bot/helper_funcs/display_progress.py
+++ bot/helper_funcs/display_progress.py
@@ -49,7 +49,7 @@ async def progress_for_pyrogram(
         elapsed_time = TimeFormatter(milliseconds=elapsed_time)
         estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
-        progress = "[{0}{1}] \n📊 <b>Progress:</b> {2}%\n".format(
+        progress = "[{0}{1}] \n♻️ <b>Pʀᴏᴄᴇssɪɴɢ:</b> {2}%\n".format(
             ''.join([FINISHED_PROGRESS_STR for i in range(math.floor(percentage / 10))]),
             ''.join([UN_FINISHED_PROGRESS_STR for i in range(10 - math.floor(percentage / 10))]),
             round(percentage, 2))
