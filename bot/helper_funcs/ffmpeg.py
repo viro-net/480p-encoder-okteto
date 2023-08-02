132c8d1a75a6cb61b7636918c5ad9e76a74de9cf
ZxScripter <131220168+ZxScripter@users.noreply.github.com>
2023-08-02 20:12:08 +0530

    ffmpeg.py

--- bot/helper_funcs/ffmpeg.py
+++ bot/helper_funcs/ffmpeg.py
@@ -49,8 +49,7 @@ async def convert_video(video_file, output_directory, total_time, bot, message, 
     preset.append("veryfast")
     audio_b.append("40k")
     watermark.append('-vf "drawtext=fontfile=font.ttf:fontsize=27:fontcolor=white:bordercolor=black@0.50:x=w-tw-10:y=10:box=1:boxcolor=black@0.5:boxborderw=6:text=@Anime_Sensei_Network"')
-    file_genertor_command = f'ffmpeg -hide_banner -loglevel quiet -progress "{progress}" -i "{video_file}" {watermark[0]}" -c:v {codec[0]} -metadata 'title=Encoded By [@Anime_Sensei_Network]' -metadata:s:v title="[@Anime_Sensei_Network]" -metadata:s:a title="[@Anime_Sensei_Network]" -metadata:s:s title="[@Anime_Sensei_Network]" -pix_fmt yuv420p -crf {crf[0]} -s {resolution[0]} -c:a libopus -b:a {audio_b[0]} -preset {preset[0]} -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 1 "{out_put_file_name}" -y'
-    #Done !!
+    file_genertor_command = f'ffmpeg -hide_banner -loglevel quiet -progress "{progress}" -i "{video_file}" {watermark[0]}  -c:v {codec[0]}  -map 0 -crf {crf[0]} -c:s copy -pix_fmt yuv420p -s {resolution[0]} -b:v 150k -c:a libopus -b:a {audio_b[0]} -preset {preset[0]}  "{out_put_file_name}" -y'
     COMPRESSION_START_TIME = time.time()
     process = await asyncio.create_subprocess_shell(
           file_genertor_command,
