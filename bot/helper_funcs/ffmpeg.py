d85c33a1e58009fab86a7e42cc9891fb757ef727
ZxScripter <131220168+ZxScripter@users.noreply.github.com>
2023-08-10 20:31:50 +0530

    ffmpeg.py

--- bot/helper_funcs/ffmpeg.py
+++ bot/helper_funcs/ffmpeg.py
@@ -1,21 +1,12 @@
 import logging
-logging.basicConfig(
-    level=logging.DEBUG, 
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-)
-LOGGER = logging.getLogger(__name__)
-
 import asyncio
 import os
-import time
 import re
 import json
 import subprocess
 import math
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from bot.helper_funcs.display_progress import (
-  TimeFormatter
-)
+from bot.helper_funcs.display_progress import TimeFormatter
 from bot.localisation import Localisation
 from bot import (
     FINISHED_PROGRESS_STR,
@@ -31,48 +22,50 @@ from bot import (
 )
 
 async def convert_video(video_file, output_directory, total_time, bot, message, chan_msg):
-    # https://stackoverflow.com/a/13891070/4723940
     kk = video_file.split("/")[-1]
     aa = kk.split(".")[-1]
     out_put_file_name = kk.replace(f".{aa}", "[@Anime_Sensei_Network].mkv")
-    #out_put_file_name = video_file + "_compressed" + ".mkv"
     progress = output_directory + "/" + "progress.txt"
     with open(progress, 'w') as f:
-      pass
-    ##  -metadata title='@Anime_Sensei_Network [Join https://t.me/Anime_Sensei_Network]' -vf drawtext=fontfile=Italic.ttf:fontsize=20:fontcolor=black:x=15:y=15:text='Anime Sensei Network'
-    ##"-metadata", "title=@Anime_Sensei_Network", "-vf", "drawtext=fontfile=njnaruto.ttf:fontsize=20:fontcolor=black:x=15:y=15:text=" "Anime Sensei Network",
-     ## -vf eq=gamma=1.4:saturation=
-    #lol 😂
+        pass
+
     crf.append("28")
     codec.append("libx264")
     resolution.append("854x480")
     preset.append("veryfast")
     audio_b.append("40k")
-    watermark.append('-vf "drawtext=fontfile=font.ttf:fontsize=27:fontcolor=white:bordercolor=black@0.50:x=w-tw-10:y=10:box=1:boxcolor=black@0.5:boxborderw=6:text=@Anime_Sensei_Network"')
-    file_genertor_command = f'ffmpeg -hide_banner -loglevel quiet -progress "{progress}" -i "{video_file}" {watermark[0]}  -c:v {codec[0]}  -map 0 -crf {crf[0]} -c:s copy -pix_fmt yuv420p -s {resolution[0]} -b:v 150k -c:a libopus -b:a {audio_b[0]} -preset {preset[0]}  "{out_put_file_name}" -y'
+    
+    metadata = "-metadata title='Anime Sensei [Join t.me/anime_sensei_network]'"
+    watermark.append(f'-vf "drawtext=fontfile=font.ttf:fontsize=27:fontcolor=white:bordercolor=black@0.50:x=w-tw-10:y=10:box=1:boxcolor=black@0.5:boxborderw=6:text=Anime Sensei {metadata}"')
+
+    file_genertor_command = (
+        f'ffmpeg -hide_banner -loglevel quiet -progress "{progress}" -i "{video_file}" {watermark[0]} '
+        f'-c:v {codec[0]} -map 0 -crf {crf[0]} -c:s copy -pix_fmt yuv420p -s {resolution[0]} -b:v 150k '
+        f'-c:a libopus -b:a {audio_b[0]} -preset {preset[0]}  "{out_put_file_name}" -y'
+    )
+
     COMPRESSION_START_TIME = time.time()
     process = await asyncio.create_subprocess_shell(
-          file_genertor_command,
-          # stdout must a pipe to be accessible as process.stdout
-           stdout=asyncio.subprocess.PIPE,
-           stderr=asyncio.subprocess.PIPE,
-          )
-    #stdout, stderr = await process.communicate()
-    
+        file_genertor_command,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE,
+    )
+
     LOGGER.info("ffmpeg_process: "+str(process.pid))
     pid_list.insert(0, process.pid)
+    
     status = output_directory + "/status.json"
     with open(status, 'r+') as f:
-      statusMsg = json.load(f)
-      statusMsg['pid'] = process.pid
-      statusMsg['message'] = message.message_id
-      f.seek(0)
-      json.dump(statusMsg,f,indent=2)
-    # os.kill(process.pid, 9)
+        statusMsg = json.load(f)
+        statusMsg['pid'] = process.pid
+        statusMsg['message'] = message.message_id
+        f.seek(0)
+        json.dump(statusMsg, f, indent=2)
+
     isDone = False
     while process.returncode != 0:
-      await asyncio.sleep(3)
-      with open(DOWNLOAD_LOCATION + "/progress.txt", 'r+') as file:
+        await asyncio.sleep(3)
+        with open(DOWNLOAD_LOCATION + "/progress.txt", 'r+') as file:
         text = file.read()
         frame = re.findall("frame=(\d+)", text)
         time_in_us=re.findall("out_time_ms=(\d+)", text)
