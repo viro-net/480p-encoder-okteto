8e2e76caea530b84f5d93b6828642e210958630d
itzmeproman <atulpandey3030@gmail.com>
2023-09-18 15:51:41 +0530

    Update incoming_message_fn.py

--- bot/plugins/incoming_message_fn.py
+++ bot/plugins/incoming_message_fn.py
@@ -36,7 +36,7 @@ from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant, Usern
 #from bot.helper_funcs.utils import(
 #  delete_downloads
 #)
-os.system("wget https://ibb.co/VYgdwms")
+os.system("wget https://i.postimg.cc/Pq3VdWys/20230906-083159.jpg")
 
 #LOGS_CHANNEL = -1001283278354
 CURRENT_PROCESSES = {}
