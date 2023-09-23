348bc8256cfa237f5f18bce311d5fa9a44bf8327
itzmeproman <atulpandey3030@gmail.com>
2023-09-23 13:11:12 +0530

    Update localisation.py

--- bot/localisation.py
+++ bot/localisation.py
@@ -16,7 +16,6 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from bot.helper_funcs.display_progress import (
   TimeFormatter
 )
-from bot.localisation import Localisation
 from bot import (
     FINISHED_PROGRESS_STR,
     UN_FINISHED_PROGRESS_STR,
