6fef283cf46784eaa39588b80734d639c58486d0
itzmeproman <atulpandey3030@gmail.com>
2023-09-23 13:03:35 +0530

    Update localisation.py

--- bot/localisation.py
+++ bot/localisation.py
@@ -23,7 +23,7 @@ class Localisation:
     
     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.95GB due to Telegram API limitations."
     
-    COMPRESS_SUCCESS = " Your anime Provider [@Anime_Compass]"
+    COMPRESS_SUCCESS = kk.replace(f".{aa}", "[@Anime_Compass].mkv")
 
     COMPRESS_PROGRESS = "🕛 ETA: {} ♻️ Progress: {}%"
 
