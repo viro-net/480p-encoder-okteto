7d93e9cd47dd5abf7c3687b354ba60287b20109e
itzmeproman <atulpandey3030@gmail.com>
2023-09-24 08:34:50 +0530

    Update incoming_message_fn.py

--- bot/plugins/incoming_message_fn.py
+++ bot/plugins/incoming_message_fn.py
@@ -343,7 +343,7 @@ async def incoming_compress_message_f(update):
         text=Localisation.UPLOAD_START,                    
       )
       u_start = time.time()
-      caption = video_file + "[@Anime_Compass]" + ".mkv" ,
+      caption = bot.helper_funcs.out_put_file_name ,
       upload = await bot.send_document(
         chat_id=update.chat.id,
         document=o,
