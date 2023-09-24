8f891f29c11f66d3fd2646e4d65fb249db0915bc
itzmeproman <atulpandey3030@gmail.com>
2023-09-24 09:23:33 +0530

    Update incoming_message_fn.py

--- bot/plugins/incoming_message_fn.py
+++ bot/plugins/incoming_message_fn.py
@@ -227,6 +227,19 @@ async def incoming_compress_message_f(update):
         )
       )
       saved_file_path = video
+      eni = saved_file_path.split("/")[-1]		
+      xnx = eni.split(".")[-1]		
+      opm = eni.replace(f".{xnx}", " .mkv")		
+      nam = opm.replace("_", " ")		
+      nam = opm.replace(".", " ")		
+      anitopy_options = {'allowed_delimiters': ' '}		
+      new_name = anitopy.parse(nam)		
+      anime_name = new_name['anime_title']		
+      episode_no = new_name['episode_number']  		
+      joined_string = f"[{anime_name}] [Episode {episode_no}] [@Anime_Compass!🧭.mkv]"		
+      if 'anime_season' in new_name.keys():		
+        animes_season = new_name['anime_season']		
+        joined_string = f"[{anime_name}] [Season {animes_season}] [Episode {episode_no}] [@Anime_Compass!🧭.mkv]"	
       LOGGER.info(saved_file_path)  
       LOGGER.info(video)
       if( video is None ):
