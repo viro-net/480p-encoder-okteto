5aa257ef9855d18d307504e8f0a3605900b130c8
veer naal <140493234+viro-net@users.noreply.github.com>
2023-11-04 22:00:58 +0530

    Update utils.py

--- bot/helper_funcs/utils.py
+++ bot/helper_funcs/utils.py
@@ -6,7 +6,7 @@ logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 )
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
-LOGGER = logging.getLogger(name)
+LOGGER = logging.getLogger(__name__)
 
 import os
 from bot import data
